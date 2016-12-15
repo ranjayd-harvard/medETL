9b32907bb93acb1406ceafb6fc4e952b3dc1b935
Ranjay Kumar <ranjayd@gmail.com>
2016-12-14 22:25:46 -0500

    Updated .env

---
+++ database/migrations/2016_12_14_041203_create_charitys_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharitysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charitys', function (Blueprint $table) {
+                  $table->increments('id');
+                  $table->string('charity_name',30)->unique();
+                  $table->string('charity_desc',1024);
+                  $table->string('address1',128);
+                  $table->string('address2',128)->nullable();
+                  $table->string('city',30);
+                  $table->string('zipcode',10);
+                  $table->string('phone1',20);
+                  $table->string('state',30);
+                  $table->string('country',30);
+                  $table->integer('user_id');          
+                  $table->timestamps();
+          });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('charitys');
+    }
+}
---
+++ database/seeds/CharitysTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class CharitysTableSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      $existingUsers = User::all();
+
+      foreach($existingUsers as $user) {
+        foreach(array(1,2) as $i) {
+            DB::table('charitys')->insert([
+              'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+              'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+              'charity_name' => $user->firstName.' - Charity '. $i,
+              'charity_desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed urna arcu. Integer sit amet risus nisi. Nulla ut consequat quam. Etiam at facilisis metus, id lobortis elit. Donec vel nulla egestas, malesuada arcu vel, pellentesque augue. Nullam quis dapibus neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;',
+              'address1' => '123 Main St',
+              'address2' => '',
+              'city' => 'Boston',
+              'state' => 'MA',
+              'zipcode' => '02110',
+              'phone1' => '(123) 456-7890',
+              'country' => 'USA',
+              'user_id' => $user->id,
+            ]);
+          }
+      }
+    }
+}
