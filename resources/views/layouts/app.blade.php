a32e716c6759faaa21689af48b190b90d9cd46ee
Ranjay Kumar <ranjayd@gmail.com>
2016-12-18 22:33:15 -0500

    fixed validator errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li>&nbsp;</li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
